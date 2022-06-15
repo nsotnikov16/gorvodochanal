c4236d82070a8ad19ab861b9d3c79aeb6ac2663e
Pavel-Lukyanov <97879761+Pavel-Lukyanov@users.noreply.github.com>
2022-06-15 16:17:21 +0500

    fix

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    //Слайдер на главной
     const swiperMain = new Swiper('.swiper__main', {
         // Optional parameters
         direction: 'horizontal',
-        loop: true,
-
+        loop: false,
+        effect: "fade",
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
@@ -16,20 +18,35 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 
+
+    //Слайдер услуги
     const swiperSecond = new Swiper('.swiper__second', {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-      
+        effect: "flip",
         // If we need pagination
         pagination: {
-          el: '.swiper-pagination',
+            el: '.swiper-pagination',
         },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
+    });
+
+
+    //Открытие меню при клике на телефон в хэдере
+    /* let headerDropdown = document.querySelector('#headerDropdown');
+    let menuDropdown = document.querySelector('#menuDropdown');
+
+    headerDropdown.addEventListener('click', function () {
+        menuDropdown.classList.add('open');
+    })
+
+    document.addEventListener('click', function(event) {
+        console.log(event.target)
+        if(event.target !== menuDropdown) {
+            menuDropdown.classList.remove('open');
+        }
+    }) */
+
 })
+
+
