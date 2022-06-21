8f57a19ee6f0b047665eb75cf6fa5268b45cc9d2
Pavel-Lukyanov <97879761+Pavel-Lukyanov@users.noreply.github.com>
2022-06-21 12:53:14 +0500

    custom select

--- js/index.js
+++ js/index.js
@@ -131,6 +131,92 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
+
+    /* Карта */
+    ymaps.ready(init);
+
+    function init() {
+        var myPlacemark,
+            myMap = new ymaps.Map('map', {
+                center: [61.241778, 73.393032],
+                zoom: 13
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
+
+        // Слушаем клик на карте.
+        myMap.events.add('click', function (e) {
+            var coords = e.get('coords');
+
+            // Если метка уже создана – просто передвигаем ее.
+            if (myPlacemark) {
+                myPlacemark.geometry.setCoordinates(coords);
+            }
+            // Если нет – создаем.
+            else {
+                myPlacemark = createPlacemark(coords);
+                myMap.geoObjects.add(myPlacemark);
+                // Слушаем событие окончания перетаскивания на метке.
+                myPlacemark.events.add('dragend', function () {
+                    getAddress(myPlacemark.geometry.getCoordinates());
+                });
+            }
+            getAddress(coords);
+        });
+
+        // Создание метки.
+        function createPlacemark(coords) {
+            return new ymaps.Placemark(coords, {
+                iconCaption: 'поиск...'
+            }, {
+                preset: 'islands#violetDotIconWithCaption',
+                draggable: true
+            });
+        }
+
+        // Определяем адрес по координатам (обратное геокодирование).
+        function getAddress(coords) {
+            myPlacemark.properties.set('iconCaption', 'поиск...');
+            ymaps.geocode(coords).then(function (res) {
+                var firstGeoObject = res.geoObjects.get(0);
+
+                myPlacemark.properties
+                    .set({
+                        // Формируем строку с данными об объекте.
+                        iconCaption: [
+                            // Название населенного пункта или вышестоящее административно-территориальное образование.
+                            firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                            // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
+                            firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+                        ].filter(Boolean).join(', '),
+                        // В качестве контента балуна задаем строку с адресом объекта.
+                        balloonContent: firstGeoObject.getAddressLine()
+                    });
+            });
+        }
+    }
+
+
+    /* Селект на странице проблемы */
+    let selects = document.querySelectorAll('.select');
+    let options = document.querySelectorAll('.select__option');
+    
+    //Открытие селекта
+    for (let select of selects) {
+        select.addEventListener('click', function (event) {
+            this.classList.toggle('open');
+            this.classList.toggle('border');
+            this.firstChild.nextElementSibling.firstChild.nextElementSibling.classList.toggle('open');
+        })
+    }
+
+    //Выбор пункта из открытого селекта
+    for(let option of options) {
+        option.addEventListener('click', function(event) {
+            event.target.parentNode.previousElementSibling.firstChild.innerHTML = option.innerHTML;
+        })
+    }
+
 })
 
 
