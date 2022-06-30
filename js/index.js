8860b3c090b87b433688955ddb58401c027ace67
Pavel-Lukyanov <97879761+Pavel-Lukyanov@users.noreply.github.com>
2022-06-30 14:54:42 +0500

    fix

--- js/index.js
+++ js/index.js
@@ -111,24 +111,51 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    //Делигирование клика на ссылки и смена контента
+
+
+    //Делигирование клика по ссылкам и смена контента
     menuLink.addEventListener('click', function (event) {
-        
-        
+        let dataLinks = document.querySelectorAll('.menu__help__content__link');
+        let i = 0;
+        for (let dataLink of dataLinks) {
+            dataLink.dataset.number = i;
+            i++;
+        }
         if (event.target.classList.contains('menu__help__content__link')) {
             event.preventDefault();
             menuAsideHead.style.visibility = "hidden";
             menuHelp.innerHTML = '<div class="menu__help__head"><span class="menu-aside__back-btn">&lt; Назад</span><button class="menu__close__icon help__close__menu" aria-label="Закрыть окно"></button></div>'
-                + `<div class="menu__aside__enter__container"><h2 class="menu__aside__head__link">${event.target.textContent}</h2>
-                
-                <p class="menu__aside__enter__text">Для оформления заявки по перекрытию/открытию/отглушению водопроводного ввода необходимо направить на эл. адрес: <a href="mailto:info@vodokanal-surgut.ru">info@vodokanal-surgut.ru</a> или привезти нарочно, гарантийное письмо на выполнение работ. Образцы заявки находятся по следующей <a href="#">ссылке</a>. Также просим обратить внимание, что при направлении заявки на "отглушение" водопроводного ввода необходимо приложить свидетельство о государственной регистрации права на объекты и на земельные участки (либо выписки из ЕГРН), а также схему с указанием точек отглушения.</p></div>`;
-            
+                + `<div class="menu__aside__enter__container"><h2 class="menu__aside__head__link">${event.target.textContent}</h2>${objAside[event.target.dataset.number]}`;
+
+            console.log(event.target.id)
             var menuBack = document.querySelector('.menu-aside__back-btn'); //Кнопка назад в боковом меню
             menuBack.addEventListener('click', function () {
                 menuHelp.innerHTML = menuDefault;
             })
         }
     })
+
+    //Объект с контентом для бокового меню
+    let objAside = {
+        0: '<p class="menu__aside__enter__text">Для оформления заявки по перекрытию/открытию/отглушению водопроводного ввода необходимо направить на эл. адрес: <a href="mailto:info@vodokanal-surgut.ru">info@vodokanal-surgut.ru</a> или привезти нарочно, гарантийное письмо на выполнение работ. Образцы заявки находятся по следующей <a href="#">ссылке</a>. Также просим обратить внимание, что при направлении заявки на "отглушение" водопроводного ввода необходимо приложить свидетельство о государственной регистрации права на объекты и на земельные участки (либо выписки из ЕГРН), а также схему с указанием точек отглушения.</p></div>',
+
+        1: '<p class="menu__aside__enter__text">Порядок подключения к централизованным системам холодного водоснабжения и/или водоотведения</p></div>',
+
+        2: '<p class="menu__aside__enter__text">Форма заявления о подключении к централизованным системам холодного водоснабжения и/или водоотведения</p></div>',
+
+        3: '<p class="menu__aside__enter__text">Для оформления заявки по перекрытию/открытию/отглушению водопроводного ввода необходимо направить на эл. адрес: <a href="mailto:info@vodokanal-surgut.ru">info@vodokanal-surgut.ru</a> или привезти нарочно, гарантийное письмо на выполнение работ. Образцы заявки находятся по следующей <a href="#">ссылке</a>. Также просим обратить внимание, что при направлении заявки на "отглушение" водопроводного ввода необходимо приложить свидетельство о государственной регистрации права на объекты и на земельные участки (либо выписки из ЕГРН), а также схему с указанием точек отглушения.</p></div>',
+
+        4: '<p class="menu__aside__enter__text">Порядок подключения к централизованным системам холодного водоснабжения и/или водоотведения</p></div>',
+
+        5: '<p class="menu__aside__enter__text">Форма заявления о подключении к централизованным системам холодного водоснабжения и/или водоотведения</p></div>',
+
+        6: '<p class="menu__aside__enter__text">Для оформления заявки по перекрытию/открытию/отглушению водопроводного ввода необходимо направить на эл. адрес: <a href="mailto:info@vodokanal-surgut.ru">info@vodokanal-surgut.ru</a> или привезти нарочно, гарантийное письмо на выполнение работ. Образцы заявки находятся по следующей <a href="#">ссылке</a>. Также просим обратить внимание, что при направлении заявки на "отглушение" водопроводного ввода необходимо приложить свидетельство о государственной регистрации права на объекты и на земельные участки (либо выписки из ЕГРН), а также схему с указанием точек отглушения.</p></div>',
+
+        7: '<p class="menu__aside__enter__text">Порядок подключения к централизованным системам холодного водоснабжения и/или водоотведения</p></div>',
+
+        8: '<p class="menu__aside__enter__text">Форма заявления о подключении к централизованным системам холодного водоснабжения и/или водоотведения</p></div>',
+
+    }
 
     //Подстановка меню в выпадающее меню
     let headerNav = document.querySelector('#headerNav');
